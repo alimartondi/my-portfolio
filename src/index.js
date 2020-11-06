fdba2a53529e5a0a9518eb71462cef1252b7a62a
ali <alimartondi88@gmail.com>
2020-11-06 22:16:21 +0700

    Improve BrowserRoute

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import NotFoundScreen from "./components/NotFound";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter forceRefresh={true}>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/about" component={AboutScreen} />
